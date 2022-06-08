c2788bdcc8b3826e2b6da0923e3e174912842c61
zerindingl <96913746+zerindingl@users.noreply.github.com>
2022-06-08 16:17:34 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ from flask import Flask, render_template, url_for, redirect, request
 from markdown2 import markdown
 from jinja2 import Environment, PackageLoader
 import os
-import glob
 import pypandoc
-from datetime import datetime 
+from datetime import datetime
 import subprocess
 import requests
-
 
 
 app = Flask(__name__)
@@ -16,50 +14,83 @@ posts = {}
 for markdown_post in os.listdir('templates'):
   if markdown_post.endswith('md'):
    file_path = os.path.join('templates', markdown_post)
-
    with open(file_path, 'r') as file:
      posts[markdown_post] = markdown(file.read(), extras=['metadata'])
      convertin = subprocess.call('./bash.sh')
+
 env = Environment(loader=PackageLoader('app', 'templates'))
 test_template = env.get_template('index.html')
 posts_metadata = [posts[post].metadata for post in posts]
 
 datas = []
-url = []
 date = []
+url = []
 lat = []
 date_list = []
-
 
 for data in posts_metadata:
  url.append(data['url'])
  url.sort()
- my_date = datetime.strptime(data['list1'], "%Y.%m.%d").date()
+ my_date = datetime.strptime(data['list1'],"%Y.%m.%d")
  time = my_date.date()
  date.append(time)
- li = list(zip(date, url))
+ li = list(zip(date,url))
  latest_article = sorted(li, key=lambda tuple:tuple[0])
  latest = latest_article[-4:]
- 
+
  d = data['list1']
  date_list.append(d)
  date_list.sort()
  new = list(zip(date_list,url))
-print(new)  
-  
- for url_post in latest:
+
+
+for url_post in latest:
   url_posts = url_post[1]
   lat.append(url_posts)
+
 for j in lat:
  for y in posts_metadata:
   if y['url'] == j:
    datas.append(y)
 
 
+def prev_page(pages):
+    for page in pages:
+     p = 0
+     global prev_url
+     prev_url = pages[p][1]
+     prev_page = pages[p][0]
+     print(prev_page)
+     if page[0] < prev_page:
+            prev_page = page[0]
+            prev_url = page[1]
+            p += 1
+    return prev_page, prev_url
+
+
+prev_page = prev_page(new)
+
+
+def next_page(pages,p):
+    for page in pages:
+     global next_url
+     next_url = page[1]
+     next_page = pages[p][0]
+     global l
+     l = []
+     if page[0] > next_page:
+            next_url = page[1]
+            l.append(next_page)
+     else:
+            p +=1
+    return next_page, next_url
+
+next_page = next_page(new,0)
+
 @app.route('/')
 def test():
  csslink = url_for('static', filename='css/main.css')
- return render_template('index.html', posts=datas,articles=datas,csslink=csslink)
+ return render_template('index.html', posts=datas, articles=datas, csslink=csslink)
 
 @app.route('/featured')
 def featured():
@@ -100,28 +131,29 @@ def xenopoetisc_archive():
 @app.route('/history1')
 def history1():
  csslink = url_for('static', filename='css/main.css')
- return render_template('history1.html',csslink=csslink, posts=posts_metadata)
+ return render_template('history1.html',csslink=csslink, posts=posts_metadata,prev_page=prev_page,next_page=next_page, prev_url=prev_url, next_url=next_url)
 
 @app.route('/history2')
 def history2():
  csslink = url_for('static', filename='css/main.css')
- return render_template('history2.html',csslink=csslink,posts=posts_metadata)
+ return render_template('history2.html',csslink=csslink,posts=posts_metadata,prev_page=prev_page,next_page=next_page, prev_url=prev_url, next_url=next_url)
 
 @app.route('/history3')
 def history3():
  csslink = url_for('static', filename='css/main.css')
- return render_template('history3.html',csslink=csslink,posts=posts_metadata)
+ return render_template('history3.html', csslink=csslink, posts=posts_metadata,prev_page=prev_page,next_page=next_page, prev_url=prev_url, next_url=next_url)
 
 @app.route('/history4')
 def history4():
  csslink = url_for('static', filename='css/main.css')
- return render_template('history4.html',csslink=csslink,posts=posts_metadata)
+ return render_template('history4.html', csslink=csslink, posts=posts_metadata,prev_page=prev_page,next_page=next_page,  prev_url=prev_url, next_url=next_url)
 
 
-@app.route('/insight')
-def insight():
+@app.route('/history6')
+def history6():
  csslink = url_for('static', filename='css/main.css')
- return render_template('insight1.md',csslink=csslink,posts=posts_metadata)
+ return render_template('history6.html', csslink=csslink, posts=posts_metadata,prev_page=prev_page,next_page=next_page ,  prev_url=prev_url,next_url=next_url)
+
 
 if __name__ == '__app__':
         app.run()
